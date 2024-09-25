918fc89bb2ad5571c373cf8c31a183e22f513a46
Vickydhakshan1999 <vcrover10@gmail.com>
2024-09-25 15:41:23 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,23 +1,18 @@
-"""
-URL configuration for Finallogin project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
+from finalone.views import  ImportExcelDataAPIView, SignInAPIView, SignupAPIView, UserCreateAPIView, UserDetailAPIView, UserUpdateAPIView,ExportUsersToExcelAPIView, UserDeleteAPIView, UserListView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/finalone/', include('finalone.urls')),
-]
+    path('signup/',SignupAPIView.as_view(), name='signup'),
+    path('signin/',SignInAPIView.as_view(), name='signin'),
+    path('user/create/', UserCreateAPIView.as_view(), name='user-create'),
+    path('user/<int:pk>/', UserDetailAPIView.as_view(), name='user-detail'),
+    path('user/update/<int:pk>/', UserUpdateAPIView.as_view(), name='user-update'),
+    path('user/delete/<int:pk>/', UserDeleteAPIView.as_view(), name='user-delete'),
+    path('users/', UserListView.as_view(), name='user-list'),
+    path('export-users/', ExportUsersToExcelAPIView.as_view(), name='export-users'),
+    path('import-users/', ImportExcelDataAPIView.as_view(), name='import-users'),
+    
+]    
+
